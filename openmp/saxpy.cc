75412ffa2f768ce90d601797b7b94441f17e7136
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-08-25 22:42:36 +0000

    OpenMP implementation.

---
+++ openmp/saxpy.cc
@@ -0,0 +1,43 @@
+#include <cstddef>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+
+int main() {
+  size_t num_elements = 1 << 20;
+  size_t buffer_size = num_elements * sizeof(float);
+
+  float *x = (float *)malloc(buffer_size);
+  float *y = (float *)malloc(buffer_size);
+  float *z = (float *)malloc(buffer_size);
+
+  const float alpha = 2.0f;
+
+  for (size_t idx = 0; idx < num_elements; idx++) {
+    x[idx] = 1.0f;
+    y[idx] = 2.0f;
+    z[idx] = 0.0f;
+  }
+
+  #pragma omp target map(x[:num_elements], y[:num_elements], z[:num_elements])
+  {
+
+    #pragma omp teams distribute parallel for
+    for (size_t idx = 0; idx < num_elements; idx++) {
+      z[idx] += alpha * x[idx] + y[idx];
+    }
+
+  }
+
+  float error = 0.0;
+  for (size_t idx = 0; idx < num_elements; idx++) {
+    error = fmax(error, fabs(z[idx] - 4.0f));
+  }
+  printf("error: %e\n", error);
+
+  free(x);
+  free(y);
+  free(z);
+
+  return 0;
+}
