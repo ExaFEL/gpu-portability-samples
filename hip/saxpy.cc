10ca1f974f91cd868d38dac92bf09ee0e3b412fd
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-08-26 15:55:10 -0700

    Fix bounds in saxpy.

--- hip/saxpy.cc
+++ hip/saxpy.cc
@@ -5,7 +5,7 @@
 __global__ void saxpy(const size_t num_elements, const float alpha,
                       const float *x, const float *y, float *z) {
   int idx = (blockIdx.x * blockDim.x) + threadIdx.x;
-  if (idx <= num_elements) z[idx] += alpha * x[idx] + y[idx];
+  if (idx < num_elements) z[idx] += alpha * x[idx] + y[idx];
 }
 
 int main() {
