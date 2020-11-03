332da796b84dc7c2ecd19c14c30fe291ac0f4751
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-11-03 23:13:38 +0000

    Update Iris configuration for Kokkos.

--- kokkos/iris_env.sh
+++ kokkos/iris_env.sh
@@ -1,9 +1,6 @@
+module load oneapi # compilers for kokkos/openmptarget_icpx
 module load cmake
-module load kokkos/openmptarget_icpx_with_host_omp # kokkos/icpx
-module load omp/2020.07.30.001 # for compiler needed by kokkoks/icpx
+module load kokkos/openmptarget_icpx
 
-export CXXFLAGS="-std=c++17 -fiopenmp -fopenmp-targets=spir64 -D__STRICT_ANSI__"
-export LDFLAGS="-fiopenmp -fopenmp-targets=spir64"
-
-# only needed for kokkos/openmptarget_icpx_with_host_omp
+# only needed for kokkos/openmptarget_icpx
 export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$KOKKOS_HOME"
