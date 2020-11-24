895ea449d4e128e5a0e4be02c9c432c5fbfc66cc
Elliott Slaughter <slaughter@cs.stanford.edu>
2020-11-24 19:56:43 +0000

    Update build flags for Kokkos on Iris.

--- kokkos/iris_env.sh
+++ kokkos/iris_env.sh
@@ -4,3 +4,4 @@ module load kokkos
 
 # only needed for kokkos
 export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$KOKKOS_HOME"
+export CXXFLAGS='-std=c++17 -fiopenmp -fopenmp-targets=spir64="-mllvm -vpo-paropt-enable-64bit-opencl-atomics=true" -Wno-openmp-mapping'
